9f54e67f0fc062bd63253d3a6f83d5623ab900ad
Derek Banister <ptownderek@yahoo.com>
2021-10-29 20:49:36 -0700

    creating models, starting with user

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
+
+class User extends Model { }
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // Validate if it is a valid email
+            validate: { isEmail: true },
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            //make sure password has to be at least 5 char
+            validate: { len: [5] },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "user",
+    }
+);
+
+module.exports = User;
