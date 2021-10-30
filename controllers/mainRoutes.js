aa3c46fdfaef4c7ac22dd9bb2925fd81d200c133
Derek Banister <ptownderek@yahoo.com>
2021-10-30 11:00:25 -0700

    root route is getting posts

--- controllers/mainRoutes.js
+++ controllers/mainRoutes.js
@@ -10,13 +10,37 @@ const { User, Post, Comment } = require("../models");
 //route for user "profile" with posts
 router.get("/", async (req, res) => {
     //find all
+    //user and comment come with post
     try {
-        console.log("uve hit this route 0")
+        const postData = await Post.findAll({
+            attributes: ["id", "title", "body", "user_id"],
+            include: [
+                {
+                    model: User,
+                    as: "user",
+                    attributes: ["username"]
+                },
+                {
+                    model: Comment,
+                    as: "comments",
+                    attributes: ["id", "comment_text", "user_id"]
+                },
+            ],
+        })
+        if (!postData) {
+            res.status(400).json({ message: "No Posts Available" });
+            return;
+        }
+        const posts = postData.map((post) => post.get({ plain: true }));
+        console.log(posts);
+        //then render to homepage
     } catch (err) {
         console.log(err);
         res.status(500).json(err)
     }
 })
+
+//will render to handlebars page later, route working though
 
 
 router.get("/viewpost/:id", (req, res) => {
@@ -42,6 +66,15 @@ router.get("/login", (req, res) => {
 router.get("/post", (req, res) => {
     try {
         console.log("uve hit this route 3")
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err)
+    }
+})
+
+router.get("/dashboard", (req, res) => {
+    try {
+        console.log("uve hit this route4")
     } catch (err) {
         console.log(err);
         res.status(500).json(err)
